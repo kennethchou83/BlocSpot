710b83a0223875f097e5c8b2baa2e69ad5cb71af
Kenneth Chou <kennethchou83@yahoo.com>
2016-05-09 21:48:59 -0700

    chapter 7 message

---
+++ WizardOfOz.m
@@ -0,0 +1,23 @@
+//
+//  WizardOfOz.m
+//  BlocExercises
+//
+//  Created by Aaron on 6/12/14.
+//
+//
+
+#import "WizardOfOz.h"
+
+@implementation WizardOfOz
+
+- (BOOL) switchesFromBlackAndWhiteToColor {
+    /* WORK HERE */
+    return NO;
+}
+
+- (NSString *) mainCharacter {
+    /* WORK HERE */
+    return @"";
+}
+
+@end
