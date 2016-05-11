2b99f755c92700e0d567812378e810530ef05d7a
Kenneth Chou <kennethchou83@yahoo.com>
2016-05-11 16:32:44 -0700

    chapter 9 message

---
+++ chapter_9/StringCheese.m
@@ -0,0 +1,38 @@
+//
+//  StringCheese.m
+//  BlocExercises
+//
+//  Created by Aaron on 6/9/14.
+//
+//
+
+#import "StringCheese.h"
+
+@implementation StringCheese
+
+- (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
+    
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+}
+
+- (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    
+    NSString *returnString = cheeseName;
+    
+    if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
+        NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch | NSBackwardsSearch];
+        returnString = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+        
+        
+        
+    }
+    return returnString;
+}
+
+- (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    NSString *cheeseString = (cheeseCount == 1 ? @"cheese" : @"cheeses");
+    return [NSString stringWithFormat:@"%d %@", cheeseCount, cheeseString];
+    
+}
+
+@end
