7991830b6877c09926cd953bdd196f1a8890323e
Kenneth Chou <kennethchou83@yahoo.com>
2016-05-11 21:27:36 -0700

    chapter 10 message

---
+++ chapter_10/EqualityDeterminer.m
@@ -0,0 +1,28 @@
+//
+//  EqualityDeterminer.m
+//  BlocExercises
+//
+//  Created by Aaron Brager on 6/10/14.
+//
+//
+
+#import "EqualityDeterminer.h"
+
+@implementation EqualityDeterminer
+
+- (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
+    /* WORK HERE */
+    return [string1 isEqualToString:string2];
+}
+
+- (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
+    /* WORK HERE */
+    return [number1 isEqualToNumber:number2];
+}
+
+- (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
+    /* WORK HERE */
+    return integer1 > integer2;
+}
+
+@end
