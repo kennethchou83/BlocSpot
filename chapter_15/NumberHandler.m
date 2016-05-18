432774d9effc56bf95664a5e6e8eb68fccd7275c
Kenneth Chou <kennethchou83@yahoo.com>
2016-05-17 17:45:34 -0700

    chapter 15 message

---
+++ chapter_15/NumberHandler.m
@@ -0,0 +1,55 @@
+//
+//  NumberHandler.m
+//  BlocExercises
+//
+//  Created by Aaron on 6/11/14.
+//
+//
+
+#import "NumberHandler.h"
+
+@implementation NumberHandler
+
+- (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
+    NSInteger integerValue = [number integerValue];
+    integerValue *= 2;
+    return @(integerValue);
+    
+    }
+
+- (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
+    if(number == otherNumber){
+        return @[@(number)];
+    }
+    NSInteger lowNumber, highNumber;
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    }else{
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    NSMutableArray *returnArray = [NSMutableArray array];
+    for(NSInteger i = lowNumber; i <= highNumber; i++){
+        [returnArray addObject:@(i)];
+    }
+    
+    
+    return returnArray;
+}
+
+- (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
+    NSInteger lowestNumber = NSIntegerMax;
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger integerValue = [number integerValue];
+    
+    
+    if (integerValue < lowestNumber) {
+        lowestNumber = integerValue;
+    }
+    }
+    return lowestNumber;
+}
+
+@end
